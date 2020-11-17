053ba2000793d2985b3a1df7b10a16a447e0cf20
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-17 10:43:43 -0800

    Added Methods to my classes

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,10 @@
 class dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting"
+    end
 end
 
 fido = dog.New
--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,11 @@
 class person
+    def talk
+        puts "Hello World!"
+    end
+    
+    def walk
+        puts "The Person is walking"
+    end
 end
 
 adele_goldberg = person.New
